0d7417ba1e209efd976eee77786fa5bc4312a023
James Lusk <jamesallenlusk@gmail.com>
2021-02-13 05:44:17 -0700

    back end connect

--- facerecon-backend/server.js
+++ facerecon-backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
